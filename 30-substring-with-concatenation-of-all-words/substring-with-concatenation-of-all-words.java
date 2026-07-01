1168c13c258186f6d39efb0949eccd27489ad754
kumarchandan001 <chandankr.62096@gmail.com>
2026-07-01 20:38:56 +0530

    Time: 12 ms (54.76%) | Memory: 47.4 MB (39.76%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        if (s == null || s.length() == 0 || words.length == 0)
+            return ans;
+
+        int wordLen = words[0].length();
+        int totalWords = words.length;
+        int windowLen = wordLen * totalWords;
+
+        if (s.length() < windowLen)
+            return ans;
+
+        HashMap<String, Integer> need = new HashMap<>();
+
+        for (String word : words) {
+            need.put(word, need.getOrDefault(word, 0) + 1);
+        }
+
+        for (int offset = 0; offset < wordLen; offset++) {
+
+            int left = offset;
+            int count = 0;
+
+            HashMap<String, Integer> window = new HashMap<>();
+
+            for (int right = offset; right + wordLen <= s.length(); right += wordLen) {
+
+                String word = s.substring(right, right + wordLen);
+
+                if (!need.containsKey(word)) {
+
+                    window.clear();
+                    count = 0;
+                    left = right + wordLen;
+
+                } else {
+
+                    window.put(word, window.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (window.get(word) > need.get(word)) {
+
+                        String leftWord = s.substring(left, left + wordLen);
+
+                        window.put(leftWord, window.get(leftWord) - 1);
+
+                        left += wordLen;
+                        count--;
+                    }
+
+                    if (count == totalWords) {
+
+                        ans.add(left);
+
+                        String leftWord = s.substring(left, left + wordLen);
+
+                        window.put(leftWord, window.get(leftWord) - 1);
+
+                        left += wordLen;
+                        count--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}

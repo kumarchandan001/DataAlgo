cad17fc938c4e7c81206642c02072d46914c4902
kumarchandan001 <chandankr.62096@gmail.com>
2026-09-06 12:47:29 +0530

    Time: 1 ms (99.89%) | Memory: 48 MB (6.83%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,18 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-
-        int j = 0;
-
-        for(int i = 0; i < nums.length; i++) {
-
-            if(nums[i] != 0) {
-
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-
-                j++;
+        int index = 0;
+        for(int element : nums){
+            if(element !=0){
+                nums[index]=element;
+                index++;
             }
+        }
+        for(int i= index; i<nums.length; i++){
+            nums[i]=0;
         }
     }
 }
